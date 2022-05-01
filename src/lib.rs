3b6229c5411f69ca55f31a75077a88d74812ce2c
Martin Abeleda <martin.abeleda@gmail.com>
2022-04-30 21:50:34 -0700

    Added binding for loads

--- src/lib.rs
+++ src/lib.rs
@@ -1,15 +1,37 @@
-
 use pyo3::prelude::*;
+use pyo3::{
+    wrap_pyfunction,
+};
+use std::str::FromStr;
+use wkt::Wkt;
 
-/// Formats the sum of two numbers as string.
+
+/// A Python module implemented in Rust.
+#[pymodule]
+fn picasso(py: Python<'_>, m: &PyModule) -> PyResult<()> {
+    m.add_function(wrap_pyfunction!(sum_as_string, m)?)?;
+    register_wkt(py, m);
+    Ok(())
+}
+
+// Formats the sum of two numbers as string.
 #[pyfunction]
 fn sum_as_string(a: usize, b: usize) -> PyResult<String> {
     Ok((a + b).to_string())
 }
 
-/// A Python module implemented in Rust.
-#[pymodule]
-fn picasso(_py: Python, m: &PyModule) -> PyResult<()> {
-    m.add_function(wrap_pyfunction!(sum_as_string, m)?)?;
+fn register_wkt(py: Python<'_>, parent_module: &PyModule) -> PyResult<()> {
+    let wkt = PyModule::new(py, "wkt")?;
+    wkt.add_function(wrap_pyfunction!(loads, wkt)?)?;
+    parent_module.add_submodule(wkt)?;
     Ok(())
+}
+
+// Load a geometry from a WKT string
+#[pyfunction]
+pub fn loads(data: String) -> PyResult<()> {
+     let wkt: Wkt<f64> = Wkt::from_str(&data).unwrap();
+     println!("Parsed obj: {:?}", wkt);
+     
+     Ok(())
 }
